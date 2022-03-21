432c510711dbff45ed7b9c61e4bb1d62d080d0f2
Manoj Rathod <rathodmanoj247@gmail.com>
2022-03-21 17:26:27 +0530

    update port no.

--- server.js
+++ server.js
@@ -42,6 +42,8 @@ app.post('/mail', (req, res) => {
     })
 })
 
-app.listen('3000', () => {
-    console.log("Listening on port 3000...");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
 })
